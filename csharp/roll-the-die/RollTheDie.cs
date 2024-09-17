43ae52bf36ab47e84dbb954eae9c8ad666014002
sravioli <sravioli+github@proton.me>
2024-09-17 10:59:35 +0200

    fix(csharp): roll-the-die, increase max_roll value

--- csharp/roll-the-die/RollTheDie.cs
+++ csharp/roll-the-die/RollTheDie.cs
@@ -5,7 +5,7 @@ public class Player
     private static readonly Random rnd = new Random();
 
     private static readonly int MIN_ROLL = 1;
-    private static readonly int MAX_ROLL = 18;
+    private static readonly int MAX_ROLL = 19;
 
     public int RollDie() => rnd.Next(MIN_ROLL, MAX_ROLL);
 
