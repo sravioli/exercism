31217ff32d99e6650ebf2995d41741328cbf2992
sravioli <sravioli+github@proton.me>
2024-09-17 10:58:01 +0200

    feat(csharp): log-analysis new iteration

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -6,7 +6,7 @@ public static class LogAnalysis
         s.Substring(s.IndexOf(delimiter) + delimiter.Length);
 
     public static string SubstringBetween(this string s, string start, string end) =>
-        s.Substring(0, s.IndexOf(end)).Substring(s.IndexOf(start) + start.Length);
+        SubstringAfter(s.Substring(0, s.IndexOf(end)), start);
 
     public static string Message(this string str) => str.SubstringAfter(": ");
 
