6d4cbedfe4c154e1c3a3dd0d28f07e0761f93901
Simone Fidanza <fsimone2002@proton.me>
2024-09-19 12:56:50 +0200

    fix(csharp): update 'booking-up-for-beauty'

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -4,7 +4,7 @@ static class Appointment
 {
     private static readonly int AFTERNOON_START = 12;
     private static readonly int AFTERNOON_END = 18;
-    private static readonly DateTime ANNIVERSARY = new DateTime(DateTime.Now.Year, 9, 15);
+    private static DateTime ANNIVERSARY = new DateTime(DateTime.Now.Year, 9, 15);
 
     public static DateTime Schedule(string appointmentDateDescription) =>
         DateTime.Parse(appointmentDateDescription);
