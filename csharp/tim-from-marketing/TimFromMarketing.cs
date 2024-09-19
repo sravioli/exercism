909f90b9da651c570d3f9f6eee93a6b3c51acfd0
Simone Fidanza <fsimone2002@proton.me>
2024-09-19 12:56:20 +0200

    fix(csharp): update 'tim-from-marketing'

--- csharp/tim-from-marketing/TimFromMarketing.cs
+++ csharp/tim-from-marketing/TimFromMarketing.cs
@@ -5,10 +5,6 @@ static class Badge
     public static string Print(int? id, string name, string? department)
     {
         string dep = department?.ToUpper() ?? "OWNER";
-        if (id == null)
-        {
-            return $"{name} - {dep}";
-        }
-        return $"[{id}] - {name} - {dep}";
+        return id == null ? $"{name} - {dep}" : $"[{id}] - {name} - {dep}";
     }
 }
