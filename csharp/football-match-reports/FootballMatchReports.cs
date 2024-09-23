6200a2794dc1997d73eec46af25a925cf3d0fa26
Simone Fidanza <fsimone2002@proton.me>
2024-09-23 10:57:47 +0200

    fix(csharp): update 'football-match-reports'

--- csharp/football-match-reports/FootballMatchReports.cs
+++ csharp/football-match-reports/FootballMatchReports.cs
@@ -2,56 +2,57 @@ using System;
 
 public static class PlayAnalyzer
 {
+    private const int GOALIE = 1;
+    private const int LEFT_BACK = 2;
+    private const int CENTER_BACK1 = 3;
+    private const int CENTER_BACK2 = 4;
+    private const int RIGHT_BACK = 5;
+    private const int MIDFIELDER_MIN = 6;
+    private const int MIDFIELDER_MAX = 8;
+    private const int LEFT_WING = 9;
+    private const int STRIKER = 10;
+    private const int RIGHT_WING = 11;
+
     public static string AnalyzeOnField(int shirtNum)
     {
-        string playerDescription;
         switch (shirtNum)
         {
-            case 1:
-                playerDescription = "goalie";
-                break;
-            case 2:
-                playerDescription = "left back";
-                break;
-            case int num when num is 3 or 4:
-                playerDescription = "center back";
-                break;
-            case 5:
-                playerDescription = "right back";
-                break;
-            case int num when num is >= 6 and <= 8:
-                playerDescription = "midfielder";
-                break;
-            case 9:
-                playerDescription = "left wing";
-                break;
-            case 10:
-                playerDescription = "striker";
-                break;
-            case 11:
-                playerDescription = "right wing";
-                break;
+            case GOALIE:
+                return "goalie";
+            case LEFT_BACK:
+                return "left back";
+            case int num when num is CENTER_BACK1 or CENTER_BACK2:
+                return "center back";
+            case RIGHT_BACK:
+                return "right back";
+            case int num when num is >= MIDFIELDER_MIN and <= MIDFIELDER_MAX:
+                return "midfielder";
+            case LEFT_WING:
+                return "left wing";
+            case STRIKER:
+                return "striker";
+            case RIGHT_WING:
+                return "right wing";
 
             default:
-                throw new ArgumentOutOfRangeException($"Shirt number is outside 1-11 range.");
+                throw new ArgumentOutOfRangeException("Shirt number is outside 1-11 range.");
         }
-        return playerDescription;
     }
 
     public static string AnalyzeOffField(object report)
     {
         switch (report)
         {
-            case int supportersNumber when report is int:
+            case int supportersNumber:
                 return $"There are {supportersNumber} supporters at the match.";
-            case string announcment when report is string:
+            case string announcment:
                 return announcment;
-            case Injury injury when report is Injury:
+            case Injury injury:
                 return $"Oh no! {injury.GetDescription()} Medics are on the field.";
-            case Incident incident when report is Incident:
+            case Incident incident:
                 return incident.GetDescription();
-            case Manager manager when report is Manager:
-                return $"{manager.Name}{(manager?.Club != null ? $" ({manager!.Club})" : $"")}";
+            case Manager manager:
+                return $"{manager.Name}{(manager?.Club != null ? $" ({manager!.Club})" : string.Empty)}";
             default:
                 throw new ArgumentException("Unknown report to analyze");
         }
