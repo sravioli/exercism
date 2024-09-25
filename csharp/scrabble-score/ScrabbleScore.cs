6e45191e49b8d23036a6555b22c3fe6f16df5c28
Simone Fidanza <fsimone2002@proton.me>
2024-09-25 12:25:53 +0200

    fix(csharp): update 'scrabble-score' solution

--- csharp/scrabble-score/ScrabbleScore.cs
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class ScrabbleScore
 {
     private static readonly int NO_SCORE = 0;
-    private static readonly Dictionary<int, List<string>> _scoreReference =
+    private static readonly Dictionary<int, string[]> _scoreReference =
         new()
         {
             { 1, ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"] },
@@ -21,9 +22,13 @@ public static class ScrabbleScore
         int score = NO_SCORE;
         foreach (char c in input)
         {
-            foreach (KeyValuePair<int, List<string>> item in _scoreReference)
+            foreach (KeyValuePair<int, string[]> item in _scoreReference)
             {
-                score += item.Value.Contains(c.ToString().ToUpper()) ? item.Key : NO_SCORE;
+                if (item.Value.Contains(c.ToString().ToUpper()))
+                {
+                    score += item.Key;
+                    break;
+                }
             }
         }
         return score;
