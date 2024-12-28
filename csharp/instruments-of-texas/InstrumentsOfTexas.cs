08aa7228f9397129df662d32d8e5d5643dbefa5f
sravioli <sravioli+github@proton.me>
2024-12-28 18:17:13 +0100

    feat(csharp): solve 'instruments-of-texas'

--- csharp/instruments-of-texas/InstrumentsOfTexas.cs
+++ csharp/instruments-of-texas/InstrumentsOfTexas.cs
@@ -1,36 +1,43 @@
 using System;
 
-public class CalculationException : Exception
+public class CalculationException(int operand1, int operand2, string message, Exception inner)
+    : Exception(message, inner)
 {
-    public CalculationException(int operand1, int operand2, string message, Exception inner)
-    // TODO: complete the definition of the constructor
-    {
-    }
-
-    public int Operand1 { get; }
-    public int Operand2 { get; }
+    public int Operand1 { get; } = operand1;
+    public int Operand2 { get; } = operand2;
 }
 
-public class CalculatorTestHarness
+public class CalculatorTestHarness(Calculator calculator)
 {
-    private Calculator calculator;
-
-    public CalculatorTestHarness(Calculator calculator)
-    {
-        this.calculator = calculator;
-    }
-
     public string TestMultiplication(int x, int y)
     {
-        throw new NotImplementedException("Please implement the CalculatorTestHarness.TestMultiplication() method");
+        try
+        {
+            Multiply(x, y);
+        }
+        catch (CalculationException e) when (e.Operand1 < 0 && e.Operand2 < 0)
+        {
+            return $"Multiply failed for negative operands. {e.Message}";
+        }
+        catch (CalculationException e) when (e.Operand1 > 0 && e.Operand2 > 0)
+        {
+            return $"Multiply failed for mixed or positive operands. {e.Message}";
+        }
+        return "Multiply succeeded";
     }
 
     public void Multiply(int x, int y)
     {
-        throw new NotImplementedException("Please implement the CalculatorTestHarness.Multiply() method");
+        try
+        {
+            calculator.Multiply(x, y);
+        }
+        catch (OverflowException ex)
+        {
+            throw new CalculationException(x, y, ex.Message, ex);
+        }
     }
 }
-
 
 // Please do not modify the code below.
 // If there is an overflow in the multiplication operation
