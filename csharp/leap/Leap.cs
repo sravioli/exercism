789a6b4bb975181e929d6af19faad7d53d711dd7
sravioli <sravioli+github@proton.me>
2024-12-29 17:29:44 +0100

    feat(csharp): solve 'leap'

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -1,9 +1,5 @@
-using System;
-
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static bool IsLeapYear(int year) =>
+        year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
