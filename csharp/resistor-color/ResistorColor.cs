49c56a0f780bba65eaeee9d9f8d80d4c222ded86
sravioli <sravioli+github@proton.me>
2024-12-29 17:56:17 +0100

    feat(csharp): solve 'resistor-color'

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -1,14 +1,23 @@
 using System;
+using System.Linq;
 
 public static class ResistorColor
 {
-    public static int ColorCode(string color)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    private static readonly string[] ColorNames =
+    [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ];
 
-    public static string[] Colors()
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static int ColorCode(string color) => Array.IndexOf(ColorNames, color);
+
+    public static string[] Colors() => ColorNames;
 }
