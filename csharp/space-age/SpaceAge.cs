86e0da3ea9e44b9fc615dcbc31f81a9ba2b28995
sravioli <sravioli+github@proton.me>
2024-12-30 00:24:56 +0100

    chore(csharp): download 'space-age'

---
+++ csharp/space-age/SpaceAge.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class SpaceAge
+{
+    public SpaceAge(int seconds)
+    {
+    }
+
+    public double OnEarth()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double OnMercury()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double OnVenus()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double OnMars()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double OnJupiter()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double OnSaturn()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double OnUranus()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public double OnNeptune()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
