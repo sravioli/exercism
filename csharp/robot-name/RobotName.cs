a78f43908386e9dda969bd933f82b914d114ed1b
sravioli <sravioli+github@proton.me>
2024-12-30 16:41:26 +0100

    chore(csharp): download 'robot-name'

---
+++ csharp/robot-name/RobotName.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Robot
+{
+    public string Name
+    {
+        get
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
