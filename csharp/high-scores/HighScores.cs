39cd784f3f791af8a32c663cd2509c18860c665c
sravioli <sravioli+github@proton.me>
2024-12-30 01:10:08 +0100

    chore(csharp): download 'high-scores'

---
+++ csharp/high-scores/HighScores.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HighScores
+{
+    public HighScores(List<int> list)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public List<int> Scores()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public int Latest()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public int PersonalBest()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+
+    public List<int> PersonalTopThree()
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
