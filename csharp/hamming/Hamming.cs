9b3a4d708ddd5c8b28c2e7a825a7bef7ae51a955
sravioli <sravioli+github@proton.me>
2024-12-30 01:26:18 +0100

    feat(csharp): solve 'hamming'

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 
 public static class Hamming
 {
-    public static int Distance(string firstStrand, string secondStrand)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static int Distance(string firstStrand, string secondStrand) =>
+        firstStrand.Length != secondStrand.Length
+            ? throw new ArgumentException("DNA strands must have the same length")
+            : firstStrand.Where((t, i) => t != secondStrand[i]).Count();
 }
