d8934612f7fd07b7ba39ee28902d49ae7967c84d
sravioli <sravioli+github@proton.me>
2025-01-01 12:44:43 +0100

    fix(csharp): update solution for exercise 'grade-school'

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -3,30 +3,24 @@ using System.Linq;
 
 public class GradeSchool
 {
-    private readonly Dictionary<int, List<string>> _roster = new();
+    private readonly SortedDictionary<int, SortedSet<string>> _roster = new();
 
     public bool Add(string student, int grade)
     {
-        if (Roster().Contains(student))
+        if (_roster.Values.Any(students => students.Contains(student)))
         {
             return false;
         }
 
-        _roster.TryGetValue(grade, out var students);
-        students ??= [];
-        students.Add(student);
-        students.Sort();
-        _roster[grade] = students;
+        if (!_roster.TryAdd(grade, [student]))
+        {
+            _roster[grade].Add(student);
+        }
         return true;
     }
 
-    public IEnumerable<string> Roster()
-    {
-        List<string> roster = [];
-        roster.AddRange(_roster.Keys.OrderBy(x => x).SelectMany(k => _roster[k]));
-        return roster;
-    }
+    public IEnumerable<string> Roster() => _roster.Values.SelectMany(students => students);
 
     public IEnumerable<string> Grade(int grade) =>
-        _roster.TryGetValue(grade, out var students) ? students.ToArray() : [];
+        _roster.TryGetValue(grade, out var students) ? students : [];
 }
