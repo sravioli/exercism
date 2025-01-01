7d3faa0fd7a581984f8f494b3530b13edf8d0c90
sravioli <sravioli+github@proton.me>
2025-01-01 15:57:33 +0100

    feat(csharp): solve 'rotational-cipher'

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,24 @@
-using System;
-
 public static class RotationalCipher
 {
+    private static readonly char[] Alphabet = ("abcdefghijklmnopqrstuvwxyz").ToCharArray();
+
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var result = new char[text.Length];
+        var charArray = text.ToCharArray();
+        for (var i = 0; i < charArray.Length; i++)
+        {
+            var ch = charArray[i];
+            if (!char.IsAsciiLetter(ch))
+            {
+                result[i] = ch;
+                continue;
+            }
+            var isUpper = char.IsUpper(ch);
+            var index = (isUpper ? char.ToLower(ch) : ch) - Alphabet[0];
+            var c = Alphabet[(index + shiftKey) % 26];
+            result[i] = isUpper ? char.ToUpper(c) : c;
+        }
+        return new string(result);
     }
 }
