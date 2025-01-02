25f08151c1a7befe345e0398863816daa5946643
sravioli <sravioli+github@proton.me>
2025-01-02 16:01:40 +0100

    feat(csharp): solve 'clock'

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -1,19 +1,37 @@
 using System;
 
-public class Clock
+public class Clock : IEquatable<Clock>
 {
-    public Clock(int hours, int minutes)
+    private const int OneHour = 60;
+    private const int OneDay = 24;
+
+    private readonly int _minutes;
+    private readonly int _hours;
+    private readonly int _time;
+
+    public Clock(int hours, int minutes = 0)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        _time = (OneHour * hours + minutes) % (OneDay * OneHour);
+        while (_time < 0)
+            _time += OneDay * OneHour;
+
+        _hours = _time / OneHour;
+        _minutes = _time % OneHour;
     }
 
-    public Clock Add(int minutesToAdd)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public Clock Add(int minutesToAdd) => new(0, _time + minutesToAdd);
 
-    public Clock Subtract(int minutesToSubtract)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public Clock Subtract(int minutesToSubtract) => Add(-minutesToSubtract);
+
+    public override string ToString() => $"{_hours:D2}:{_minutes:D2}";
+
+    public bool Equals(Clock other) =>
+        other is not null
+        && (ReferenceEquals(this, other) || _minutes == other._minutes && _hours == other._hours);
+
+    public override bool Equals(object obj) =>
+        obj is not null
+        && (ReferenceEquals(this, obj) || obj.GetType() == GetType() && Equals((Clock)obj));
+
+    public override int GetHashCode() => HashCode.Combine(_minutes, _hours);
 }
