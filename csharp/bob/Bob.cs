5b3e021e764c3fbc08ccde9f03349f01f7d5d4f7
sravioli <sravioli+github@proton.me>
2025-01-03 22:11:51 +0100

    feat(csharp): solve 'bob'

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,24 @@
-using System;
+using System.Linq;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (string.IsNullOrEmpty(statement) || string.IsNullOrWhiteSpace(statement))
+            return "Fine. Be that way!";
+
+        var s = statement.Trim();
+        var isAskingQuestion = s[^1] == '?';
+        var isShouting =
+            s.Any(c => !(char.IsPunctuation(c) || char.IsDigit(c)) && char.IsAsciiLetter(c))
+            && s.Where(char.IsAsciiLetter).All(char.IsAsciiLetterUpper);
+
+        return isShouting switch
+        {
+            true when isAskingQuestion => "Calm down, I know what I'm doing!",
+            true => "Whoa, chill out!",
+            false when isAskingQuestion => "Sure.",
+            _ => "Whatever.",
+        };
     }
 }
