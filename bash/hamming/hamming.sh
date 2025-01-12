cb4294e4b9442705f97b85280fd5dfc5ecac1879
sravioli <sravioli+github@proton.me>
2025-01-12 23:13:34 +0100

    feat(bash): solve 'hamming'

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -22,3 +22,44 @@
 #   main "$@"
 #
 # *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+
+declare -gA NUCLEOTIDES=(
+  ["C"]=true
+  ["A"]=true
+  ["G"]=true
+  ["T"]=true
+)
+
+function is_nucleotide() {
+  [[ "$1" == "C" ]] || [[ "$1" == "A" ]] || [[ "$1" == "G" ]] || [[ "$1" == "T" ]]
+}
+
+function main() {
+  if [[ "$#" -lt 2 ]]; then
+    echo "Usage: hamming.sh <string1> <string2>"
+    exit 1
+  fi
+
+  local first second
+  first="$1"
+  second="$2"
+
+  if [[ "${#first}" != "${#second}" ]]; then
+    echo "strands must be of equal length"
+    return 1
+  fi
+
+  declare -i count=0
+  for ((i = 0; i < ${#first}; i++)); do
+    ch1="${first:$i:1}"
+    ch2="${second:$i:1}"
+
+    if [[ "${ch1}" != "${ch2}" ]]; then
+      count+=1
+    fi
+  done
+
+  echo "${count}"
+}
+
+main "$@"
