8a4de073b098e76cfbe96e11b1da19c6409212ad
sravioli <sravioli+github@proton.me>
2025-01-12 22:51:35 +0100

    fix(bash): update solution for exercise 'raindrops'

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -2,29 +2,20 @@
 
 function main() {
   local result=""
-  local is_divisible=false
 
   if (($1 % 3 == 0)); then
     result="${result}Pling"
-    is_divisible=true
   fi
 
   if (($1 % 5 == 0)); then
     result="${result}Plang"
-    is_divisible=true
   fi
 
   if (($1 % 7 == 0)); then
     result="${result}Plong"
-    is_divisible=true
   fi
 
-  if [[ "${is_divisible}" == false ]]; then
-    echo "$1"
-    return 0
-  fi
-
-  echo "${result}"
+  echo "${result:-"$1"}"
   return 0
 }
 
