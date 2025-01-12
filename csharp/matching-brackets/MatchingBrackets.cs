5098749cdfd7460be12eb1cdf7eac1c98157cf4a
sravioli <sravioli+github@proton.me>
2025-01-12 16:28:31 +0100

    feat(csharp): solve 'matching-brackets'

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class MatchingBrackets
 {
+    private static readonly Dictionary<char, char> Brackets = new()
+    {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+    };
+
     public static bool IsPaired(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (string.IsNullOrWhiteSpace(input))
+            return true;
+
+        Stack<char> stack = new();
+        foreach (var c in input.Where(c => Brackets.ContainsKey(c) || Brackets.ContainsValue(c)))
+        {
+            if (Brackets.TryGetValue(c, out var value))
+            {
+                stack.Push(value);
+                continue;
+            }
+
+            if (!stack.TryPeek(out var bracket))
+                return false;
+
+            if (c != bracket)
+                return false;
+
+            stack.Pop();
+        }
+
+        return stack.Count == 0;
     }
 }
