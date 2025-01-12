2bfd89ca9d8e6dece9dd6d847efb0ba7f42caf55
sravioli <sravioli+github@proton.me>
2025-01-12 22:49:34 +0100

    feat(bash): solve 'raindrops'

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+function main() {
+  local result=""
+  local is_divisible=false
+
+  if (($1 % 3 == 0)); then
+    result="${result}Pling"
+    is_divisible=true
+  fi
+
+  if (($1 % 5 == 0)); then
+    result="${result}Plang"
+    is_divisible=true
+  fi
+
+  if (($1 % 7 == 0)); then
+    result="${result}Plong"
+    is_divisible=true
+  fi
+
+  if [[ "${is_divisible}" == false ]]; then
+    echo "$1"
+    return 0
+  fi
+
+  echo "${result}"
+  return 0
+}
+
+main "$@"
