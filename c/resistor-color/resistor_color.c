b1e1ce6933206f19166bdeb03c743c52006df86a
sravioli <sravioli+github@proton.me>
2025-01-14 22:46:14 +0100

    fix(c): update solution for exercise 'resistor-color'

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -2,11 +2,10 @@
 
 #include <stdlib.h>
 
-int color_code(resistor_band_t resistor) { return (int)resistor; }
+int color_code(resistor_band_t resistor) { return resistor; }
 
 resistor_band_t *colors() {
-  resistor_band_t *colors =
-    (resistor_band_t *)malloc(sizeof(resistor_band_t) * ((int)WHITE + 1));
+  static resistor_band_t colors[WHITE + 1];
   for (resistor_band_t color = BLACK; color <= WHITE; color++) {
     colors[color] = color;
   }
