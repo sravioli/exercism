4b0bc60724b737e1824f7a84280e0703093a0863
sravioli <sravioli+github@proton.me>
2025-01-14 22:43:10 +0100

    feat(c): solve 'resistor-color'

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,1 +1,14 @@
 #include "resistor_color.h"
+
+#include <stdlib.h>
+
+int color_code(resistor_band_t resistor) { return (int)resistor; }
+
+resistor_band_t *colors() {
+  resistor_band_t *colors =
+    (resistor_band_t *)malloc(sizeof(resistor_band_t) * ((int)WHITE + 1));
+  for (resistor_band_t color = BLACK; color <= WHITE; color++) {
+    colors[color] = color;
+  }
+  return colors;
+}
--- c/resistor-color/resistor_color.h
+++ c/resistor-color/resistor_color.h
@@ -2,7 +2,20 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-
+  BLACK = 0,
+  BROWN = 1,
+  RED = 2,
+  ORANGE = 3,
+  YELLOW = 4,
+  GREEN = 5,
+  BLUE = 6,
+  VIOLET = 7,
+  GREY = 8,
+  WHITE = 9,
 } resistor_band_t;
+
+int color_code(resistor_band_t resistor);
+
+resistor_band_t *colors();
 
 #endif
