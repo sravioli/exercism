94e14bd1993a8d13cfcec6181961dafd91d89d86
sravioli <sravioli+github@proton.me>
2025-01-14 22:53:03 +0100

    feat(c): solve 'collatz-conjecture'

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -1,1 +1,15 @@
 #include "collatz_conjecture.h"
+
+int steps(int start) {
+  if (start <= 0) {
+    return -1;
+  }
+
+  int steps = 0;
+  while (start > 1) {
+    start = start % 2 == 0 ? start / 2 : (start * 3) + 1;
+    steps++;
+  }
+
+  return steps;
+}
