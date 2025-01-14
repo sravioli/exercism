eb1370132a8828a4d5910f7d7964abbcb87683cd
sravioli <sravioli+github@proton.me>
2025-01-14 22:07:51 +0100

    feat(c): solve 'grains'

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,27 @@
 #include "grains.h"
+
+#define MAX_SQUARES 64
+
+uint64_t square(uint8_t index) {
+  if (index > MAX_SQUARES) {
+    return 0;
+  } else if (index <= 2) {
+    return index;
+  }
+
+  uint64_t sq = 2;
+  for (int i = 2; i < index; i++) {
+    sq *= 2;
+  }
+  return sq;
+}
+
+uint64_t total(void) {
+  uint64_t sq = 2;
+  uint64_t sum = 3;  // 1st + 2nd
+  for (int i = 1; i <= MAX_SQUARES; i++) {
+    sq *= 2;
+    sum += sq;
+  }
+  return sum;
+}
