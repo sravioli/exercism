fece3182dc08001787e13369ef1242e09517d393
sravioli <sravioli+github@proton.me>
2025-01-19 23:09:25 +0100

    feat(lua): solve 'resistor-color-duo'

--- lua/resistor-color-duo/resistor-color-duo.lua
+++ lua/resistor-color-duo/resistor-color-duo.lua
@@ -1,4 +1,24 @@
+local resistor_colors = {
+  ["black"] = 0,
+  ["brown"] = 1,
+  ["red"] = 2,
+  ["orange"] = 3,
+  ["yellow"] = 4,
+  ["green"] = 5,
+  ["blue"] = 6,
+  ["violet"] = 7,
+  ["grey"] = 8,
+  ["white"] = 9,
+}
+
 return {
   value = function(colors)
-  end
+    local num = ""
+
+    for i = 1, 2 do -- max two items
+      num = num .. resistor_colors[colors[i]]
+    end
+
+    return tonumber(num)
+  end,
 }
