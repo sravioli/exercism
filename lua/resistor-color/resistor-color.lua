ef2decb4276aef6b6c0a30e3002a12c64e5ff3d0
sravioli <sravioli+github@proton.me>
2025-01-19 23:00:24 +0100

    feat(lua): solve 'resistor-color'

--- lua/resistor-color/resistor-color.lua
+++ lua/resistor-color/resistor-color.lua
@@ -1,4 +1,18 @@
+local resistor_colors = {
+  ["black"] = 0,
+  ["brown"] = 1,
+  ["red"] = 2,
+  ["orange"] = 3,
+  ["yellow"] = 4,
+  ["green"] = 5,
+  ["blue"] = 6,
+  ["violet"] = 7,
+  ["grey"] = 8,
+  ["white"] = 9,
+}
+
 return {
   color_code = function(color)
-  end
+    return resistor_colors[color]
+  end,
 }
