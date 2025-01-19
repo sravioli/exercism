84856546891323a105ed81e76d0d4dbb42d34108
sravioli <sravioli+github@proton.me>
2025-01-19 22:57:08 +0100

    feat(lua): solve 'raindrops'

--- lua/raindrops/raindrops.lua
+++ lua/raindrops/raindrops.lua
@@ -1,2 +1,17 @@
 return function(n)
+  local drop = ""
+
+  if n % 3 == 0 then
+    drop = drop .. "Pling"
+  end
+
+  if n % 5 == 0 then
+    drop = drop .. "Plang"
+  end
+
+  if n % 7 == 0 then
+    drop = drop .. "Plong"
+  end
+
+  return #drop == 0 and tostring(n) or drop
 end
