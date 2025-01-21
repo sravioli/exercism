3e9da1c8710d0f355245926c626532a44914ac28
sravioli <sravioli+github@proton.me>
2025-01-21 21:39:35 +0100

    fix(c): update solution for exercise 'queen-attack'

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,17 +1,24 @@
 #include "queen_attack.h"
 
-attack_status_t can_attack(position_t queen_1, position_t queen_2) {
-  uint64_t x1 = queen_1.column, y1 = queen_1.row;
-  uint64_t x2 = queen_2.column, y2 = queen_2.row;
+#include <stdlib.h>
 
-  int is_col_equal = x1 == x2;
-  int is_row_equal = y1 == y2;
-  if (x1 > 7 || y1 > 7 || x2 > 7 || y2 > 7 || (is_col_equal && is_row_equal)) {
+#define MAX_COLS 8
+#define MAX_ROWS 8
+
+inline static int on_board(position_t queen) {
+  return queen.column < MAX_COLS && queen.row < MAX_ROWS;
+}
+
+attack_status_t can_attack(position_t queen_1, position_t queen_2) {
+  int x1 = queen_1.column, y1 = queen_1.row;
+  int x2 = queen_2.column, y2 = queen_2.row;
+
+  int dx = abs(x1 - x2), dy = abs(y1 - y2);
+  if (!on_board(queen_1) || !on_board(queen_2) || dx + dy == 0) {
     return INVALID_POSITION;
   }
 
-  int are_on_diagonal = (x1 - x2) == (y1 - y2) || (x1 + y1) == (x2 + y2);
-  if (is_col_equal || is_row_equal || are_on_diagonal) {
+  if (dx * dy == 0 || dx == dy) {
     return CAN_ATTACK;
   }
 
