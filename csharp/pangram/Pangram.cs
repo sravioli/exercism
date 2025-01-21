fe5d5b7d9622523783b2b21d6503a128a0f33203
sravioli <sravioli+github@proton.me>
2025-01-21 23:25:56 +0100

    fix(csharp): update solution for exercise 'pangram'

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,23 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 public static class Pangram
 {
     private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    public static bool IsPangram(string input)
-    {
-        var sb = new StringBuilder();
-        input
-            .Where(char.IsLetter)
-            .Select(char.ToLower)
-            .Distinct()
-            .OrderBy(c => c)
-            .ToList()
-            .ForEach(c => sb.Append(c));
-
-        return string.Equals(sb.ToString(), Alphabet, StringComparison.InvariantCulture);
-    }
+    public static bool IsPangram(string input) =>
+        input.ToLower().Where(char.IsLetter).GroupBy(c => c).Count() == 26;
 }
