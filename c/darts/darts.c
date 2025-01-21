28bfe75f977042cec64a7e43f7fb720efa14223d
sravioli <sravioli+github@proton.me>
2025-01-21 22:03:33 +0100

    feat(c): solve 'darts'

--- c/darts/darts.c
+++ c/darts/darts.c
@@ -1,1 +1,17 @@
 #include "darts.h"
+
+#include <math.h>
+
+uint8_t score(coordinate_t coord) {
+  float distance = sqrt(coord.x * coord.x + coord.y * coord.y);
+
+  if (distance >= 0 && distance <= 1) {
+    return 10;
+  } else if (distance > 1 && distance <= 5) {
+    return 5;
+  } else if (distance > 5 && distance <= 10) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
--- c/darts/darts.h
+++ c/darts/darts.h
@@ -1,4 +1,13 @@
 #ifndef DARTS_H
 #define DARTS_H
 
+#include <stdint.h>
+
+typedef struct coordinate_t {
+  float x;
+  float y;
+} coordinate_t;
+
+uint8_t score(coordinate_t position);
+
 #endif
