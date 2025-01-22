032129b023781239366b6b378ef22dd75ccf0531
sravioli <sravioli+github@proton.me>
2025-01-22 22:22:19 +0100

    feat(csharp): solve 'acronym'

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Linq;
+using System.Text;
 
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        var sb = new StringBuilder();
+        var words = phrase.Replace("-", " ").Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        foreach (var word in words)
+        {
+            sb.Append(char.ToUpper(word.First(char.IsAsciiLetter)));
+        }
+        return sb.ToString();
     }
 }
