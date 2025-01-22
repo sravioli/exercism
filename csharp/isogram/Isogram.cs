28ede13c1ac8eb741686c928adf4626534c03550
sravioli <sravioli+github@proton.me>
2025-01-22 20:17:32 +0100

    fix(csharp): update solution for exercise 'isogram'

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -1,24 +1,10 @@
-using System.Collections.Generic;
 using System.Linq;
 
 public static class Isogram
 {
     public static bool IsIsogram(string word)
     {
-        char[] appeared = new char[word.Length];
-        for (int i = 0; i < word.Length; i++)
-        {
-            var c = char.ToLower(word[i]);
-
-            if (!char.IsAsciiLetter(c))
-                continue;
-
-            if (appeared.Contains(c))
-                return false;
-
-            appeared[i] = c;
-        }
-
-        return true;
+        var letters = word.Where(char.IsLetterOrDigit).Select(char.ToLower).ToArray();
+        return letters.Distinct().Count() == letters.Length;
     }
 }
