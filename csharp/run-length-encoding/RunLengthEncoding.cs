4ba20d47af9fd437b26f3aea6fd312f6e377fdc4
sravioli <sravioli+github@proton.me>
2025-01-23 22:32:58 +0100

    feat(csharp): solve 'run-length-encoding'

--- csharp/run-length-encoding/RunLengthEncoding.cs
+++ csharp/run-length-encoding/RunLengthEncoding.cs
@@ -1,14 +1,49 @@
-using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 public static class RunLengthEncoding
 {
     public static string Encode(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+
+        var sb = new StringBuilder();
+        var last = input[0];
+        var count = 0;
+        foreach (var c in input)
+        {
+            if (last == c)
+            {
+                count++;
+                continue;
+            }
+
+            sb.Append(count > 1 ? count : "");
+            sb.Append(last);
+            count = 1;
+            last = c;
+        }
+        sb.Append(count > 1 ? count : "");
+        sb.Append(last);
+
+        return sb.ToString();
     }
 
     public static string Decode(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+
+        var sb = new StringBuilder();
+        foreach (Match match in Regex.Matches(input, @"(\d*)(.)"))
+        {
+            GroupCollection groups = match.Groups;
+            var num = int.Parse(groups[1].Value == string.Empty ? "1" : groups[1].Value);
+            var letter = char.Parse(match.Groups[2].Value);
+            sb.Append(letter, num);
+        }
+
+        return sb.ToString();
     }
 }
