4cb6a32a96a2834794ced29111a94e20aca9256f
sravioli <sravioli+github@proton.me>
2025-01-23 22:42:08 +0100

    feat(csharp): solve 'reverse-string'

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -1,9 +1,8 @@
 using System;
+using System.Linq;
 
 public static class ReverseString
 {
-    public static string Reverse(string input)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static string Reverse(string input) =>
+        input.ToCharArray().Reverse().Aggregate(string.Empty, (current, c) => current + c);
 }
