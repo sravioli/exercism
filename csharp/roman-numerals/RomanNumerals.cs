89f00bd8f9e99148281bd20f2f4e9f3f45625bd8
sravioli <sravioli+github@proton.me>
2025-01-26 12:06:12 +0100

    feat(csharp): solve 'roman-numerals'

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,9 +1,37 @@
-using System;
+using System.Collections.Generic;
+using System.Text;
 
 public static class RomanNumeralExtension
 {
+    private static readonly Dictionary<int, string> RomanNumerals = new()
+    {
+        { 1000, "M" },
+        { 900, "CM" },
+        { 500, "D" },
+        { 400, "CD" },
+        { 100, "C" },
+        { 90, "XC" },
+        { 50, "L" },
+        { 40, "XL" },
+        { 10, "X" },
+        { 9, "IX" },
+        { 5, "V" },
+        { 4, "IV" },
+        { 1, "I" },
+    };
+
     public static string ToRoman(this int value)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var output = new StringBuilder();
+        var remainder = value;
+        foreach (var (threshold, numeral) in RomanNumerals)
+        {
+            while (remainder / threshold > 0)
+            {
+                remainder -= threshold;
+                output.Append(numeral);
+            }
+        }
+        return output.ToString();
     }
 }
