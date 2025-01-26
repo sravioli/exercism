313774ce1aaef1de4c9f40e49a9de483240c0304
sravioli <sravioli+github@proton.me>
2025-01-26 12:13:35 +0100

    feat(csharp): solve 'collatz-conjecture'

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,14 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(number);
+
+        var steps = 0;
+        while (number > 1)
+        {
+            number = number % 2 == 0 ? number / 2 : (3 * number) + 1;
+            steps++;
+        }
+        return steps;
     }
 }
