f47257d953fe925ba3a299eb7d0ef066f35e5881
sravioli <sravioli+github@proton.me>
2025-01-26 12:46:55 +0100

    feat(csharp): solve 'triangle'

--- csharp/triangle/Triangle.cs
+++ csharp/triangle/Triangle.cs
@@ -2,18 +2,18 @@ using System;
 
 public static class Triangle
 {
-    public static bool IsScalene(double side1, double side2, double side3)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    private static bool IsValid(double a, double b, double c) =>
+        !(a.Equals(0) || b.Equals(0) || c.Equals(0)) && (a + b >= c && b + c >= a && a + c >= b);
 
-    public static bool IsIsosceles(double side1, double side2, double side3) 
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static bool IsScalene(double side1, double side2, double side3) =>
+        IsValid(side1, side2, side3)
+        && (!side1.Equals(side2) && !side2.Equals(side3) && !side3.Equals(side1));
 
-    public static bool IsEquilateral(double side1, double side2, double side3) 
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+    public static bool IsIsosceles(double side1, double side2, double side3) =>
+        IsValid(side1, side2, side3)
+        && (side1.Equals(side2) || side2.Equals(side3) || side3.Equals(side1));
+
+    public static bool IsEquilateral(double side1, double side2, double side3) =>
+        IsValid(side1, side2, side3)
+        && (side1.Equals(side2) && side2.Equals(side3) && side3.Equals(side1));
 }
