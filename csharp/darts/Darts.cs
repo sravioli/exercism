8350e793a6f5973897dc2f28055b1601a03bf153
sravioli <sravioli+github@proton.me>
2025-01-26 12:22:06 +0100

    fix(csharp): update solution for exercise 'darts'

--- csharp/darts/Darts.cs
+++ csharp/darts/Darts.cs
@@ -2,16 +2,12 @@ using System;
 
 public static class Darts
 {
-    public static int Score(double x, double y)
-    {
-        var hypotenuse = Math.Sqrt(x * x + y * y);
-
-        return hypotenuse switch
+    public static int Score(double x, double y) =>
+        Math.Sqrt(x * x + y * y) switch
         {
             <= 10 and > 5 => 1,
             <= 5 and > 1 => 5,
             <= 1 => 10,
             _ => 0,
         };
-    }
 }
