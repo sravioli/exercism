854854ca03a4e2d9975dce7eb4996c767204cdb6
sravioli <sravioli+github@proton.me>
2025-01-26 12:55:32 +0100

    feat(csharp): solve 'raindrops'

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Text;
 
 public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var sb = new StringBuilder();
+
+        if (number % 3 == 0)
+            sb.Append("Pling");
+
+        if (number % 5 == 0)
+            sb.Append("Plang");
+
+        if (number % 7 == 0)
+            sb.Append("Plong");
+
+        return sb.Length == 0 ? number.ToString() : sb.ToString();
     }
 }
