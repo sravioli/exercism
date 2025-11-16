1a140af70b7629d1e862f6d6f56ea1c04b8ef10d
Simone Fidanza <s.fidanza@pm.me>
2025-11-16 23:43:25 +0100

    feat(csharp): solve 'perfect-numbers'

--- csharp/perfect-numbers/PerfectNumbers.cs
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -1,14 +1,33 @@
 public enum Classification
 {
-    Perfect,
-    Abundant,
-    Deficient
+  Perfect,
+  Abundant,
+  Deficient
 }
+
+// The aliquot sum is defined as the sum of the factors of a number not
+// including the number itself. For example, the aliquot sum of 15 is 1 + 3 + 5 = 9.
 
 public static class PerfectNumbers
 {
-    public static Classification Classify(int number)
+  public static Classification Classify(int number)
+  {
+    ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(number, 0);
+
+    if (number == 1)
+      return Classification.Deficient;
+
+    var sum = Enumerable
+      .Range(1, number - 1)
+      .Where(x => number % x == 0)
+      .Sum();
+
+    return number switch
     {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+      int n when n == sum => Classification.Perfect,
+      int n when n < sum => Classification.Abundant,
+      int n when n > sum => Classification.Deficient,
+      _ => throw new InvalidOperationException(),
+    };
+  }
 }
