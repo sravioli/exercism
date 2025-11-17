76c2ff96a7b5f398361e5fd5d7ace2952367e42b
Simone Fidanza <s.fidanza@pm.me>
2025-11-17 22:25:28 +0100

    chore(csharp): download 'resistor-color-trio'

---
+++ csharp/resistor-color-trio/ResistorColorTrio.cs
@@ -0,0 +1,7 @@
+public static class ResistorColorTrio
+{
+    public static string Label(string[] colors)
+    {
+        throw new NotImplementedException("You need to implement this method.");
+    }
+}
