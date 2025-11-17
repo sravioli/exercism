3d8a94b81f13f62c75a04249269b070805e58a18
Simone Fidanza <s.fidanza@pm.me>
2025-11-17 22:23:43 +0100

    feat(csharp): solve 'resistor-color-duo'

--- csharp/resistor-color-duo/ResistorColorDuo.cs
+++ csharp/resistor-color-duo/ResistorColorDuo.cs
@@ -1,7 +1,18 @@
+using System.Text;
+
 public static class ResistorColorDuo
 {
-    public static int Value(string[] colors)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+  private enum ResistorColors { Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White }
+
+  public static int Value(string[] colors)
+  {
+    var sb = new StringBuilder();
+
+    foreach (var c in colors.Take(2))
+      sb.Append((int)Enum.Parse<ResistorColors>(c, ignoreCase: true));
+
+    return !int.TryParse(sb.ToString(), out int result)
+      ? throw new InvalidOperationException()
+      : result;
+  }
 }
--- csharp/resistor-color-duo/ResistorColorDuoTests.cs
+++ csharp/resistor-color-duo/ResistorColorDuoTests.cs
@@ -6,37 +6,37 @@ public class ResistorColorDuoTests
         Assert.Equal(10, ResistorColorDuo.Value(["brown", "black"]));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Blue_and_grey()
     {
         Assert.Equal(68, ResistorColorDuo.Value(["blue", "grey"]));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Yellow_and_violet()
     {
         Assert.Equal(47, ResistorColorDuo.Value(["yellow", "violet"]));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void White_and_red()
     {
         Assert.Equal(92, ResistorColorDuo.Value(["white", "red"]));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Orange_and_orange()
     {
         Assert.Equal(33, ResistorColorDuo.Value(["orange", "orange"]));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Ignore_additional_colors()
     {
         Assert.Equal(51, ResistorColorDuo.Value(["green", "brown", "orange"]));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Black_and_brown_one_digit()
     {
         Assert.Equal(1, ResistorColorDuo.Value(["black", "brown"]));
